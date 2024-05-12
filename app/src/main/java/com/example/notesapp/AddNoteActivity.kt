5af7f82936ac68fa0c12f3978216efa3e17acea2
Himansa2002 <IT22219398@my.sliit.lk>
2024-05-12 16:47:55 +0530

    first change

---
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.kt
@@ -0,0 +1,35 @@
+package com.example.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.databinding.ActivityAddNoteBinding
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding // Declare binding variable
+    private lateinit var  db : NoteDatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddNoteBinding.inflate(layoutInflater) // Initialize binding
+        setContentView(binding.root) // Set content view using binding.root
+
+        db= NoteDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+
+            val title = binding.titleEditText.text.toString()
+            val content =binding.contentEditText.text.toString()
+            val note =Note(0,title,content)
+
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"Note Saved", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/notesapp/UpdateActivity.kt
@@ -0,0 +1,53 @@
+package com.example.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.notesapp.databinding.ActivityUpdateBinding
+
+class UpdateActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityUpdateBinding
+    private lateinit var db : NoteDatabaseHelper
+    private  var noteId : Int = -1
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        db = NoteDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+
+        if(noteId==-1){
+
+            finish()
+            return
+        }
+
+        val note = db.getNoteBYID(noteId)
+        binding.titleUpdateEditText.setText(note.title)
+         binding.contentUpdateEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener{
+
+            val newTitle= binding.titleUpdateEditText.text.toString()
+            val newContent = binding.contentUpdateEditText.text.toString()
+            val updatedNote = Note (noteId,newTitle,newContent)
+           db.updateNote(updatedNote)
+
+            finish()
+           Toast.makeText(this,"Changes Saved", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+
+
+
+    }
+}
